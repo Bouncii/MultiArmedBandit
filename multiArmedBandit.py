24a9fdff3c177ca6c2d2dcc59243474f65bbbea0
bounci <lulubouncer@gmail.com>
2026-02-10 21:09:23 +0100

    epsilon greedy first attempt

--- multiArmedBandit.py
+++ multiArmedBandit.py
@@ -1,34 +1,74 @@
 import numpy as np
 
+
 class Bras:
     def __init__(self, possibilites, probas):
-        self.possibilites = possibilites
-        self.probas = probas
-
+        self.possibilites = np.array(possibilites)
+        self.probas = np.array(probas)
+        self.moyenne = np.sum(self.possibilites * self.probas)
 
     def pull(self):
         return np.random.choice(self.possibilites, p=self.probas)
-
-
-
-
-
 
 class MultiArmedBandit:
     def __init__(self, bras):
         self.bras = bras
 
     def pull_bras(self, bras_index):
-        if 0 <= bras_index < len(self.bras):
-            return self.bras[bras_index].pull()
-        print("Bras invalide")
+        return self.bras[bras_index].pull()
 
 
-bras_0 = Bras([0.3, 0.2, 0.0], [0.9, 0.02, 0.08])
-bras_1 = Bras([0.1, 0.5, 0.9], [0.2, 0.3, 0.5])
-bras_2 = Bras([1.0, 0.0], [0.1, 0.9])
+class EpsilonGreedy:
+    def __init__(self, n_arms, epsilon=0.1):
+        self.n_arms = n_arms
+        self.epsilon = epsilon
+        
+        self.counts = np.zeros(n_arms)
+        self.values = np.zeros(n_arms)
 
-bandit = MultiArmedBandit([bras_0, bras_1, bras_2])
+    def select_arm(self):
+        if np.random.random() < self.epsilon:
+            return np.random.randint(self.n_arms)
+        else:
+            return np.argmax(self.values)
 
-for i in range(10):
-    print(bandit.pull_bras(2))
+    def update(self, arm_index, reward):
+        self.counts[arm_index] += 1
+        n = self.counts[arm_index]
+        
+        moy = self.values[arm_index]
+        new_moy = moy + (1 / n) * (reward - moy)
+        self.values[arm_index] = new_moy
+
+
+
+
+
+bras_0 = Bras([0.05, 0.1, 0.15, 0.2 ], [0.3, 0.4, 0.2, 0.1])
+bras_1 = Bras([0.1, 0.12, 0.9], [0.45, 0.5, 0.05])
+bras_2 = Bras([0.0, 0.3], [0.5, 0.5])
+
+bras_list = [bras_0, bras_1, bras_2]
+bandit = MultiArmedBandit(bras_list)
+agent = EpsilonGreedy(3, 0.1)
+
+moyennes = [b.moyenne for b in bras_list]
+meilleure_moyenne = max(moyennes)
+meilleur_bras_theorique = np.argmax(moyennes)
+
+n_iterations = 1000
+regret = 0
+for i in range(n_iterations):
+    bras = agent.select_arm()
+    reward = bandit.pull_bras(bras)
+    agent.update(bras, reward)
+
+    regret += meilleure_moyenne - bras_list[bras].moyenne
+
+print("Résultats après", n_iterations, "itérations :")
+for i, val in enumerate(agent.values):
+    print(f"Bras {i} : Estimé = {val} | Tiré {int(agent.counts[i])} fois")
+
+best_arm = np.argmax(agent.values)
+print(f"L'agent pense que le meilleur bras est le n°{best_arm}")
+print(f"le regret est de : {regret}")
